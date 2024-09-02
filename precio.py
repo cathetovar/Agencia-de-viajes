3eaa348425c274399cd276ae0e3a666fd7e62f50
Catherine Tovar Riobo <cathetovar@hotmail.com>
2024-09-02 02:04:32 -0500

    Update precio.py

--- precio.py
+++ precio.py
@@ -1,19 +1,41 @@
+from utilities import diasTotales
 
-#Elegir paquete y numero de personas y precio total 
-def choose (paquetes):
-    choose=int(input("\nIngrese el numero correspondiente al paquete que desea: "))
-    number=int(input("Digite la cantidad de personas que adquiriran el paquete: "))
-    paquete=paquetes[choose-1]
-    salidachoose=[]
-    valor= paquete[6]
-    #Eliminar los puntos 
-    valorc=valor.replace(".","")
-    precio=int(valorc)*number
-    #volver a agregar los puntos
-    precioc="{:,}".format(precio).replace(",", ".")
-        
-    print("El precio a pagar seria de: $", precioc)
-    print()
-    salidachoose=[precioc,number, paquete]
-    return salidachoose     
 
+#Elegir paquete y numero de personas y precio total
+def choose(paquetes, fechasReserva):
+    while True:
+        try:
+            paqueteIndex = int(input("\nIngrese el número correspondiente al paquete que desea: ")) - 1
+            if paqueteIndex < 0 or paqueteIndex >= len(paquetes):
+                raise ValueError("Índice de paquete fuera de rango.")
+            break
+        except ValueError as e:
+            print(f"Entrada no válida: {e}. Por favor, intente de nuevo.")
+
+    while True:
+        try:
+            numPersonas = int(input("Digite la cantidad de personas que adquirirán el paquete: "))
+            if numPersonas <= 0:
+                raise ValueError("El número de personas debe ser mayor que cero.")
+            break
+        except ValueError as e:
+            print(f"Entrada no válida: {e}. Por favor, intente de nuevo.")
+
+    paquete = paquetes[paqueteIndex]
+    precioUnitario = int(paquete[6].replace(".", ""))
+    precioUnitarioTotalYDias = calcularPrecioUnitarioTotal(precioUnitario, fechasReserva)
+    precioFinal = precioUnitarioTotalYDias[0] * numPersonas
+
+    precioFormateado = "{:,}".format(precioFinal).replace(",", ".")
+
+    print("El precio a pagar por los "+str(precioUnitarioTotalYDias[1])+f" sería de: ${precioFormateado}\n")
+    return [precioFormateado, numPersonas, paquete]
+
+def calcularPrecioUnitarioTotal(precioPorNoche, fechasReserva):
+    # Calcular la diferencia de días entre las fechas
+    diferenciaDias = diasTotales(fechasReserva)
+
+    # Calcular el precio total
+    precioTotal = precioPorNoche * diferenciaDias
+
+    return [precioTotal,diferenciaDias]
