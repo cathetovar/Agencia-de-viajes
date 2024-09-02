61e3bf472624cc4ffba1114b1e2e617ec8da9d6d
Catherine Tovar Riobo <cathetovar@hotmail.com>
2024-09-02 02:06:10 -0500

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,13 +1,17 @@
-#funciones utilitarias 
+import datetime
+
+#funciones utilitarias
 #imprimir listas
 def printList(inLista):
     for lista in inLista:
             for elemento in lista:
                 print(elemento, end=" ")
             print()  # Salto de línea después de cada lista
+
 #imprimir lista como un solo string
 def printSingleList(inLista):
      print(" ".join(inLista))
+
 #imprimir datos de pasajeros
 def printPasajeros(pasajeros):
     i=0
@@ -20,3 +24,13 @@ def printPasajeros(pasajeros):
         print(pasajero[2]+": "+pasajero[3])
         print("Email: ",pasajero[4])
         print()
+
+def diasTotales(fechasReserva):
+    # Convertir las fechas de string a objetos datetime
+    fechaSalida = datetime.datetime.strptime(fechasReserva[0], "%d/%m/%Y")
+    fechaLlegada = datetime.datetime.strptime(fechasReserva[1], "%d/%m/%Y")
+
+    # Calcular la diferencia de días entre las fechas
+    diferenciaDias = (fechaLlegada - fechaSalida).days
+
+    return diferenciaDias
