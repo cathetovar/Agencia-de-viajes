13a3186b2ece15ce31b04ade6c75d0c37d8722ee
Catherine Tovar Riobo <cathetovar@hotmail.com>
2024-09-02 02:02:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,34 @@
 from origenydestino import validarorigenydestino
 from paquetes import paquetesListado, printpaquete
 from precio import choose
-from gestion_reserva import datospasajeros, reserva
+from gestion_reserva import datosPasajeros, reserva, confirmarReserva
 from constantes import *
 from fechas_viajes import fechasViajes
-from gestion_pagos import gestionarPago
 
 while True:
-    #consultar origen y destino
-    origendestino=validarorigenydestino()
+    # Consultar origen y destino
+    origendestino = validarorigenydestino()
     #Arreglo apra guardar el origen y destino ingresados por el cliente
-    destino=origendestino[1]
+    destino = origendestino[1]
 
-    #Ingresar fechas para el viaje
-    fechasReserva=fechasViajes()
+    # Ingresar fechas para el viaje
+    fechasReserva = fechasViajes()
 
-    #Buscar paquetes disponibles al usuario de acuerdo al destino ingresado
-    paquetes=paquetesListado(destino)
+    # Buscar paquetes disponibles al usuario de acuerdo al destino ingresado
+    paquetes = paquetesListado(destino)
 
-    #Imprimir los paquetes correspondientes
+    # Imprimir los paquetes correspondientes
     printpaquete(paquetes)
 
-    #seleccionar y Calcular precio
-    precionumberpaquete=choose(paquetes)
+    # Seleccionar y calcular precio
+    precioPaquete = choose(paquetes,fechasReserva)
 
-    #Se consulta los datos de los pasajeros.
-    pasajeros=datospasajeros(precionumberpaquete[1])
+    # Consultar los datos de los pasajeros
+    pasajeros = datosPasajeros(precioPaquete[1])
 
-    #Gestion de reserva
-    reservaOut=reserva(origendestino,precionumberpaquete,ciudades,pasajeros,fechasReserva)
+    # Gestión de reserva
+    reservaOut = reserva(origendestino, precioPaquete, ciudades, pasajeros, fechasReserva)
     print("¿Es correcta su reserva?")
-    confirmReserva=input("1. SI\n2. NO\nDigite el numero correspondiente: ")
 
-    if int(confirmReserva) == 1:
-        print("\nGracias por confirmar, estas son nuestras opciones de pago")
-        gestionarPago(precionumberpaquete[0])
-        print("\nGracias por comprar con nosotros, pronto te llegara a tu telefono y correo la confirmacion de tu compra. \nTen un viaje espectacular.\n")
+    if confirmarReserva(precioPaquete[0]):
         break
-    elif int(confirmReserva)==2:
-        print("Muy bien volvamos a empezar")
-    else:
-        print("No ingresaste un caracter valido, pero igual volvamos a empezar.")
-
