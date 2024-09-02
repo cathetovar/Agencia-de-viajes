bbd7b5ddf021904f5f6966ee28b521f619f16461
Catherine Tovar Riobo <cathetovar@hotmail.com>
2024-09-02 02:03:35 -0500

    Update origenydestino.py

--- origenydestino.py
+++ origenydestino.py
@@ -1,45 +1,27 @@
 #Funcion de Registro
-#validacion origen
-def validacionorigen ():
+# Función común de validación para origen y destino
+def validarLocacion(prompt):
     while True:
         try:
-            origen = int(input("\nDigite el numero de su lugar de partida: "))
-            if origen >=1 and origen<=5:
-                break
+            location = int(input(prompt))
+            if 1 <= location <= 5:
+                return location
             else:
                 print("Por favor, introduce un número válido.")
         except ValueError:
             print("Por favor, introduce un número válido.")
-    return origen
-#validacion destino
-def validadestino ():
-    while True:
-        try:
-            destino=int(input("\nDigite el numero de lugar de llegada: "))
-            if destino >=1 and destino<=5:
-                break
-            else:
-                print("Por favor, introduce un número válido.")
-        except ValueError:
-            print("Por favor, introduce un número válido.")
-    return destino
 
 #orquestación origen y destino
-def validarorigenydestino():       
-    print("Bienvenido a la agencia de viajes panchito la mejor agencia de viajes en Colombia, a continuacion se mostraran las cidades donde estamos ubicados: ")
-
-    print(" 1. Bogota \n 2. Medellin \n 3. Cartagena \n 4. Barranquilla \n 5. Cali")
+def validarorigenydestino():
+    print("-----------------------------------------------------------\n")
+    print("Bienvenido a la agencia de viajes Panchito, la mejor agencia de viajes en Colombia. A continuación se mostrarán las ciudades donde estamos ubicados:")
+    print(" 1. Bogotá \n 2. Medellín \n 3. Cartagena \n 4. Barranquilla \n 5. Cali")
 
     while True:
-        try:
-            origen = int(validacionorigen())
-            destino = int(validadestino())
-            
-            if origen != destino:
-                lista=[origen,destino]
-                break
-            else:
-                print("El destino y el origen no pueden ser iguales")  
-        except:
-            print("El destino y el origen no pueden ser iguales")
-    return lista
+        origen = validarLocacion("\nDigite el número de su lugar de partida: ")
+        destino = validarLocacion("\nDigite el número de lugar de llegada: ")
+
+        if origen != destino:
+            return [origen, destino]
+        else:
+            print("El destino y el origen no pueden ser iguales.")
