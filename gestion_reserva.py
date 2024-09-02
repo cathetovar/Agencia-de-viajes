ab465caebdd47d41acae3d934a13e55d19b15206
Catherine Tovar Riobo <cathetovar@hotmail.com>
2024-09-02 02:03:15 -0500

    Update gestion_reserva.py

--- gestion_reserva.py
+++ gestion_reserva.py
@@ -1,36 +1,55 @@
+from gestion_pagos import mostrarOpcionesPago
 from utilities import *
 
-#datos de los pasajeros
-def datospasajeros(number):
-    pasajeros=[]
+# Datos de los pasajeros
+def datosPasajeros(number):
+    pasajeros = []
     for i in range(number):
-        a=i+1
-        print("Ingresa los datos del pasajero ",a, ":")
-        nombre=input("Nombre: ")
-        apellido=input("Apellido: ")
-        tipo=input("Tipo de documento (TI, CC, PP): ")
-        id=input("Numero de documento: ")
-        telefono=input("Telefono: ")
-        correo=input("Correo electronico: ")
+        a = i + 1
+        print("Ingresa los datos del pasajero ", a, ":")
+        nombre = input("Nombre: ")
+        apellido = input("Apellido: ")
+        tipo = input("Tipo de documento (TI, CC, PP): ")
+        documentoId = input("Número de documento: ")
+        telefono = input("Teléfono: ")
+        correo = input("Correo electrónico: ")
 
         # Usar append para añadir el pasajero a la lista
-        pasajeros.append([nombre, apellido, tipo, id, telefono, correo])
+        pasajeros.append([nombre, apellido, tipo, documentoId, telefono, correo])
     return pasajeros
-#imprimir y almacenar el resumen la reserva 
-def reserva(origendestino,precionumberpaquete,ciudades,pasajeros,fechasReserva):
-    origen=ciudades[origendestino[0]-1]
-    destino=ciudades[origendestino[1]-1]
-    print("\n")
-    print("Su reserva seria: ")
+
+# Imprimir y almacenar el resumen de la reserva
+def reserva(origenDestino, precioNumberPaquete, ciudades, pasajeros, fechasReserva):
+    origen = ciudades[origenDestino[0] - 1]
+    destino = ciudades[origenDestino[1] - 1]
+    print("---------------------------------------\n")
+    print("Su reserva sería: ")
     print("Origen:", origen, "\nDestino: ", destino)
     print("\nLas fechas de su reserva son")
-    print("Fecha de salida: ",fechasReserva[0])
-    print("Fecha de llegada: ",fechasReserva[1])
-    paquete=precionumberpaquete[2]
+    print("Fecha de salida: ", fechasReserva[0])
+    print("Fecha de llegada: ", fechasReserva[1])
+    paquete = precioNumberPaquete[2]
     print("\nPaquete: ")
     printSingleList(paquete)
-    precioc=precionumberpaquete[0]
-    print("\nPrecio total: ",precioc)
+    precioC = precioNumberPaquete[0]
+    print("\nPrecio total: $", precioC)
     printPasajeros(pasajeros)
-    reservaUsuario=[origendestino,precionumberpaquete,ciudades,pasajeros]
+    reservaUsuario = [origenDestino, precioNumberPaquete, ciudades, pasajeros]
     return reservaUsuario
+
+def confirmarReserva(precioPaquete):
+    while True:
+        confirmReserva = input("Confirma tu reserva (1 para confirmar, 2 para reiniciar): ")
+        if confirmReserva.isdigit():
+            confirmReserva = int(confirmReserva)
+            if confirmReserva == 1:
+                mostrarOpcionesPago(precioPaquete)
+                break
+            elif confirmReserva == 2:
+                print("Muy bien, volvamos a empezar")
+                return False
+            else:
+                print("No ingresaste un carácter válido, pero igual volvamos a empezar.")
+        else:
+            print("Entrada no válida. Por favor, ingresa un número.")
+    return True
