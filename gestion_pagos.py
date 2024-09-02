7a39afbe94a149f70944954bd866c3a9a098700c
Catherine Tovar Riobo <cathetovar@hotmail.com>
2024-09-02 02:03:02 -0500

    Update gestion_pagos.py

--- gestion_pagos.py
+++ gestion_pagos.py
@@ -1,7 +1,10 @@
-
-#Se muestran opciones de pagos
-def gestionarPago(precioc):
+# Se muestran opciones de pagos
+def gestionarPago(precioC):
     print("\n Para gestionar su pago tenemos los siguientes canales: ")
     print("Nequi o Daviplata al 3113125245")
     print("Una vez realizado su pago envie el comprobante de pago al 3113125245")
-    
+
+def mostrarOpcionesPago(precio):
+    print("\nGracias por confirmar, estas son nuestras opciones de pago")
+    gestionarPago(precio)
+    print("\nGracias por comprar con nosotros, pronto te llegará a tu teléfono y correo la confirmación de tu compra. \nTen un viaje espectacular.\n")
